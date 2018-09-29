4e861d9e0115e67df448556a9d0409e48a2d810b
Darren <darren.oyk@gmail.com>
2018-09-29 23:31:00 +0800

    Indent with 4 spaces instead of 2

--- src/main/java/seedu/address/model/staff/Staff.java
+++ src/main/java/seedu/address/model/staff/Staff.java
@@ -15,34 +15,34 @@ public class Staff {
   private Role role;
 
   public enum Role {
-    admin, manager, user
+      admin, manager, user
   }
 
   /**
    * Every field must be present and not null.
    */
   private Staff(Username username, Password password, Name name, Role role) {
-    requireAllNonNull(username, password, name, role);
-    this.username = username;
-    this.password = password;
-    this.name = name;
-    this.role = role;
+      requireAllNonNull(username, password, name, role);
+      this.username = username;
+      this.password = password;
+      this.name = name;
+      this.role = role;
   }
 
   public Username getUsername() {
-    return this.username;
+      return this.username;
   }
 
   public Password getPassword() {
-    return this.password;
+      return this.password;
   }
 
   public Name getName() {
-    return this.name;
+      return this.name;
   }
 
   public Role getRole() {
-    return this.role;
+      return this.role;
   }
 
   /**
@@ -51,34 +51,34 @@ public class Staff {
    */
   @Override
   public boolean equals(Object other) {
-    if (!(other instanceof Staff)) {
-      return false;
-    }
+      if (!(other instanceof Staff)) {
+          return false;
+      }
 
-    if (other == this) {
-      return true;
-    }
+      if (other == this) {
+          return true;
+      }
 
-    Staff otherStaff = (Staff) other;
-    return otherStaff.getUsername().equals(getUsername()) &&
-        otherStaff.getPassword().equals(getPassword()) &&
-        otherStaff.getName().equals(getName()) &&
-        otherStaff.getRole().equals(getRole());
+      Staff otherStaff = (Staff) other;
+      return otherStaff.getUsername().equals(getUsername()) &&
+          otherStaff.getPassword().equals(getPassword()) &&
+          otherStaff.getName().equals(getName()) &&
+          otherStaff.getRole().equals(getRole());
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(username, password, name, role);
+      return Objects.hash(username, password, name, role);
   }
 
   @Override
   public String toString() {
-    final StringBuilder builder = new StringBuilder();
-    builder.append(getUsername())
-        .append(" Name: ")
-        .append(getName())
-        .append(" Role: ")
-        .append(getRole());
-    return builder.toString();
+      final StringBuilder builder = new StringBuilder();
+      builder.append(getUsername())
+          .append(" Name: ")
+          .append(getName())
+          .append(" Role: ")
+          .append(getRole());
+      return builder.toString();
   }
 }
