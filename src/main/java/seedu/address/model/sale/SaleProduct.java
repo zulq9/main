e09612aacb357be3d57d6c5735cfe328da957839
EsmondTan <esmond@equotix.com>
2018-10-02 21:43:43 +0800

    more SKU fixes

--- src/main/java/seedu/address/model/sale/SaleProduct.java
+++ src/main/java/seedu/address/model/sale/SaleProduct.java
@@ -12,7 +12,7 @@ public class SaleProduct {
     public static final String MESSAGE_SKU_CONSTRAINTS = "Product SKU should be alphanumeric and not empty.";
     public static final String SKU_VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
-    public final String productSKU;
+    public final String productSku;
 
     /**
      * Constructs a {@code SaleProduct}.
@@ -22,7 +22,7 @@ public class SaleProduct {
     public SaleProduct(String productSku) {
         requireNonNull(productSku);
         checkArgument(isValidProductSku(productSku), MESSAGE_SKU_CONSTRAINTS);
-        this.productSKU = productSku;
+        this.productSku = productSku;
     }
 
     /**
@@ -36,19 +36,19 @@ public class SaleProduct {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof SaleProduct // instanceof handles nulls
-                && productSKU.equals(((SaleProduct) other).productSKU)); // state check
+                && productSku.equals(((SaleProduct) other).productSku)); // state check
     }
 
     @Override
     public int hashCode() {
-        return productSKU.hashCode();
+        return productSku.hashCode();
     }
 
     /**
      * Format state as text for viewing.
      */
     public String toString() {
-        return '[' + productSKU + ']';
+        return '[' + productSku + ']';
     }
 
 }
