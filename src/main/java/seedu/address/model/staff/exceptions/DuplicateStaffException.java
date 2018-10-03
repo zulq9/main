275e1b9962bfc4a7f8883c131190f1cc2fc3981c
Darren <darren.oyk@gmail.com>
2018-10-04 00:22:53 +0800

    Add exceptions into Staff Model
    The exceptions added are necessary to be set up before the StaffList to be constructed.

---
+++ src/main/java/seedu/address/model/staff/exceptions/DuplicateStaffException.java
@@ -0,0 +1,11 @@
+package seedu.address.model.staff.exceptions;
+
+/**
+ * Signals that the operation will result in duplicate Staff (Staffs are considered duplicates if they have the same
+ * identity).
+ */
+public class DuplicateStaffException extends RuntimeException {
+    public DuplicateStaffException() {
+        super("Operation would result in duplicate staffs");
+    }
+}
