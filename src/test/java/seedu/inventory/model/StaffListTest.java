24d56e189b683675cb64e6638ee91e5833268476
Darren <darren.oyk@gmail.com>
2018-10-05 17:51:21 +0800

    StaffListTest.java: add more test cases

--- src/test/java/seedu/inventory/model/StaffListTest.java
+++ src/test/java/seedu/inventory/model/StaffListTest.java
@@ -3,6 +3,9 @@ package seedu.inventory.model;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static seedu.inventory.logic.commands.CommandTestUtil.VALID_NAME_ZUL;
+import static seedu.inventory.logic.commands.CommandTestUtil.VALID_PASSWORD_ZUL;
+import static seedu.inventory.logic.commands.CommandTestUtil.VALID_USERNAME_ZUL;
 import static seedu.inventory.testutil.TypicalStaffs.ZUL;
 import static seedu.inventory.testutil.TypicalStaffs.getTypicalStaffList;
 
@@ -18,6 +21,7 @@ import org.junit.rules.ExpectedException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import seedu.inventory.model.staff.Staff;
+import seedu.inventory.model.staff.UniqueStaffList;
 import seedu.inventory.model.staff.exceptions.DuplicateStaffException;
 import seedu.inventory.testutil.StaffBuilder;
 
@@ -27,6 +31,14 @@ public class StaffListTest {
     public ExpectedException thrown = ExpectedException.none();
 
     private final StaffList staffList = new StaffList();
+
+    @Test
+    public void constructor_withReadOnlyStaffList() {
+        StaffList expectedStaffList = getTypicalStaffList();
+        StaffListStub listStub = new StaffListStub(expectedStaffList.getStaffList());
+        StaffList staffListWithParams = new StaffList(listStub);
+        assertEquals(expectedStaffList, staffListWithParams);
+    }
 
     @Test
     public void constructor() {
@@ -99,9 +111,28 @@ public class StaffListTest {
     }
 
     @Test
+    public void updateStaff() {
+        staffList.addStaff(ZUL);
+        Staff editedZul = new StaffBuilder().withName(VALID_NAME_ZUL).withUsername(VALID_USERNAME_ZUL)
+                .withPassword(VALID_PASSWORD_ZUL).build();
+        StaffList expectedStaffList = new StaffList();
+        expectedStaffList.addStaff(editedZul);
+        staffList.updateStaff(ZUL, editedZul);
+        assertEquals(expectedStaffList, staffList);
+    }
+
+    @Test
     public void removeStaff_nullKey_throwsNullPointerException() {
         thrown.expect(NullPointerException.class);
         staffList.removeStaff(null);
+    }
+
+    @Test
+    public void removeStaff() {
+        staffList.addStaff(ZUL);
+        StaffList expectedStaffList = new StaffList();
+        staffList.removeStaff(ZUL);
+        assertEquals(expectedStaffList, staffList);
     }
 
     /**
