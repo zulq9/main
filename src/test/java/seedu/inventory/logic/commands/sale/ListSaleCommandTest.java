7d3f8d72236405edba9cdd56742cde235b32f62f
darren96 <darren.oyk@gmail.com>
2018-10-23 20:54:17 +0800

    ListSaleCommandTest.java: Remove SaleList

--- src/test/java/seedu/inventory/logic/commands/sale/ListSaleCommandTest.java
+++ src/test/java/seedu/inventory/logic/commands/sale/ListSaleCommandTest.java
@@ -10,7 +10,6 @@ import seedu.inventory.logic.CommandHistory;
 import seedu.inventory.model.Model;
 import seedu.inventory.model.ModelManager;
 import seedu.inventory.model.SaleList;
-import seedu.inventory.model.StaffList;
 import seedu.inventory.model.UserPrefs;
 
 /**
@@ -24,8 +23,8 @@ public class ListSaleCommandTest {
 
     @Before
     public void setUp() {
-        model = new ModelManager(getTypicalInventory(), new UserPrefs(), new SaleList(), new StaffList());
-        expectedModel = new ModelManager(model.getInventory(), new UserPrefs(), new SaleList(), new StaffList());
+        model = new ModelManager(getTypicalInventory(), new UserPrefs(), new SaleList());
+        expectedModel = new ModelManager(model.getInventory(), new UserPrefs(), new SaleList());
     }
 
     @Test
