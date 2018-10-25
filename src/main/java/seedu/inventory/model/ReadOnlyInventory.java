ac0be80b347605d392f3348203189715e3c171d5
WANG CHAO <1229983126@qq.com>
2018-10-25 16:43:37 +0800

    Change the structure of model

--- src/main/java/seedu/inventory/model/ReadOnlyInventory.java
+++ src/main/java/seedu/inventory/model/ReadOnlyInventory.java
@@ -1,26 +1,12 @@
 package seedu.inventory.model;
 
-import javafx.collections.ObservableList;
-
 import seedu.inventory.model.item.Item;
-import seedu.inventory.model.purchaseorder.PurchaseOrder;
+
 /**
  * Unmodifiable view of an inventory
  */
 
 public interface ReadOnlyInventory extends ReadOnlyItemList, ReadOnlyPurchaseOrderList, ReadOnlyStaffList {
-
-
-    /**
-     * Returns an unmodifiable view of the items list.
-     * This list will not contain any duplicate items.
-     */
-    ObservableList<Item> getItemList();
-
-    /**
-     * Returns an unmodifiable view of the purchase order list.
-     */
-    ObservableList<PurchaseOrder> getPurchaseOrderList();
 
 
     /**
