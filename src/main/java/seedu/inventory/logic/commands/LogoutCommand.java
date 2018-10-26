cbd314e628f5790039f89740958f1fb0f9345180
darren96 <darren.oyk@gmail.com>
2018-10-27 01:02:54 +0800

    Add Logout.java

---
+++ src/main/java/seedu/inventory/logic/commands/LogoutCommand.java
@@ -0,0 +1,41 @@
+package seedu.inventory.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.inventory.logic.CommandHistory;
+import seedu.inventory.logic.commands.exceptions.CommandException;
+import seedu.inventory.model.Model;
+
+/**
+ * Logs the user out from the system.
+ */
+public class LogoutCommand extends Command {
+    public static final String COMMAND_WORD = "logout";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Log an user out from the system.";
+
+    public static final String MESSAGE_SUCCESS = "You have successfully logged out from the system.";
+    public static final String MESSAGE_FAILED = "You are not logged in.";
+
+    public LogoutCommand() {
+
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+
+        if (!model.isUserLoggedIn()) {
+            throw new CommandException(MESSAGE_FAILED);
+        }
+
+        model.logoutUser();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || other instanceof LogoutCommand;
+    }
+}
