31b8cfadef90764bb1557f33a3fa51918368ee53
WANG CHAO <1229983126@qq.com>
2018-10-27 17:00:59 +0800

    Csv adpated po csv serialable stafflist test finished

--- src/test/java/seedu/inventory/storage/CsvSerializableStaffListTest.java
+++ src/test/java/seedu/inventory/storage/CsvSerializableStaffListTest.java
@@ -6,78 +6,56 @@ import static org.junit.Assert.assertNotEquals;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import seedu.inventory.commons.exceptions.IllegalValueException;
 import seedu.inventory.commons.util.CsvUtil;
-import seedu.inventory.model.Inventory;
-import seedu.inventory.model.SaleList;
-import seedu.inventory.model.item.Item;
-import seedu.inventory.model.item.Quantity;
-import seedu.inventory.model.sale.Sale;
-import seedu.inventory.model.sale.SaleDate;
-import seedu.inventory.model.sale.SaleId;
-import seedu.inventory.testutil.TypicalItems;
-//TODO
+import seedu.inventory.model.ReadOnlyStaffList;
+import seedu.inventory.testutil.staff.TypicalStaffs;
+
 public class CsvSerializableStaffListTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "CsvSerializableStaffListTest");
-    private static final Path TYPICAL_SALES_FILE = TEST_DATA_FOLDER.resolve("typicalSaleList.csv");
-    private static final Path INVALID_SALE_FILE = TEST_DATA_FOLDER.resolve("invalidSaleList.csv");
-    private static final Path DUPLICATE_SALE_FILE = TEST_DATA_FOLDER.resolve("duplicateSaleList.csv");
-
-    private static SaleId saleId = new SaleId("1");
-    private static Item item = TypicalItems.IPHONE;
-    private static Quantity quantity = new Quantity("1");
-    private static SaleDate saleDate = new SaleDate("2018-08-01");
+    private static final Path TYPICAL_STAFFS_FILE = TEST_DATA_FOLDER.resolve("typicalStaffList.csv");
+    private static final Path INVALID_STAFF_FILE = TEST_DATA_FOLDER.resolve("invalidStaffList.csv");
+    private static final Path DUPLICATE_STAFF_FILE = TEST_DATA_FOLDER.resolve("duplicateStaffList.csv");
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    private SaleList typicalSaleList = new SaleList();
-    private Inventory typicalInventory = TypicalItems.getTypicalInventory();
-
-    @Before
-    public void setUp() {
-        typicalSaleList.addSale(new Sale(saleId, item, quantity, saleDate));
+    @Test
+    public void toModelType_typicalStaffsFile_success() throws Exception {
+        CsvSerializableStaffList dataFromFile = new CsvSerializableStaffList(CsvUtil
+                .getDataFromFile(TYPICAL_STAFFS_FILE, new CsvSerializableStaffList()));
+        ReadOnlyStaffList staffListFromFile = dataFromFile.toModelType();
+        ReadOnlyStaffList typicalStaffsStaffList = TypicalStaffs.getTypicalStaffList();
+        assertEquals(staffListFromFile.getStaffList(), typicalStaffsStaffList.getStaffList());
     }
 
     @Test
-    public void toModelType_typicalSalesFile_success() throws Exception {
-        CsvSerializableSaleList dataFromFile = new CsvSerializableSaleList(CsvUtil.getDataFromFile(TYPICAL_SALES_FILE,
-                new CsvSerializableSaleList()));
-        dataFromFile.setInventory(typicalInventory);
-        SaleList saleListFromFile = dataFromFile.toModelType();
-        assertEquals(saleListFromFile, typicalSaleList);
-        assertEquals(dataFromFile, new CsvSerializableSaleList(typicalSaleList));
-    }
-
-    @Test
-    public void toModelType_invalidSaleFile_throwsIllegalValueException() throws Exception {
-        CsvSerializableSaleList dataFromFile = new CsvSerializableSaleList(CsvUtil.getDataFromFile(INVALID_SALE_FILE,
-                new CsvSerializableSaleList()));
-        dataFromFile.setInventory(typicalInventory);
+    public void toModelType_invalidStaffFile_throwsIllegalValueException() throws Exception {
+        CsvSerializableStaffList dataFromFile = new CsvSerializableStaffList(CsvUtil
+                .getDataFromFile(INVALID_STAFF_FILE, new CsvSerializableStaffList()));
         thrown.expect(IllegalValueException.class);
         dataFromFile.toModelType();
     }
 
     @Test
-    public void toModelType_duplicateSales_throwsIllegalValueException() throws Exception {
-        CsvSerializableSaleList dataFromFile = new CsvSerializableSaleList(CsvUtil.getDataFromFile(DUPLICATE_SALE_FILE,
-                new CsvSerializableSaleList()));
-        dataFromFile.setInventory(typicalInventory);
+    public void toModelType_duplicateStaffs_throwsIllegalValueException() throws Exception {
+        CsvSerializableStaffList dataFromFile = new CsvSerializableStaffList(CsvUtil
+                .getDataFromFile(DUPLICATE_STAFF_FILE, new CsvSerializableStaffList()));
         thrown.expect(IllegalValueException.class);
-        thrown.expectMessage(CsvSerializableSaleList.MESSAGE_DUPLICATE_SALE);
+        thrown.expectMessage(CsvSerializableStaffList.MESSAGE_DUPLICATE_STAFF);
         dataFromFile.toModelType();
     }
 
     @Test
     public void equals() {
-        CsvSerializableSaleList saleList = new CsvSerializableSaleList();
-        assertEquals(saleList, saleList);
-        assertNotEquals(saleList, new CsvSerializableItemList());
+        CsvSerializableStaffList staffList = new CsvSerializableStaffList();
+        assertEquals(staffList, staffList);
+        assertNotEquals(staffList, new CsvSerializableItemList());
+        assertNotEquals(staffList, new CsvSerializableStaffList(TypicalStaffs.getTypicalStaffList()));
     }
 }
